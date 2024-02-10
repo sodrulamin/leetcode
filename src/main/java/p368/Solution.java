4a28a27667d774bdecdc2c8236ab10d50e9f9279
Shaon, Amin <amin.shaon@alibaba-inc.com>
2024-02-10 22:14:58 +0600

    368. Largest Divisible Subset

---
+++ src/main/java/p368/Solution.java
@@ -0,0 +1,49 @@
+package p368;
+
+import java.util.*;
+
+/**
+ * @author Sodrul Amin Shaon
+ * {@code @created} 10/02/2024
+ */
+class Solution {
+
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        int[] groupSize = new int[n];
+        int[] prevElement = new int[n];
+        int maxIndex = 0;
+
+        for(int i=0;i<n;i++) {
+            groupSize[i] = 1;
+            prevElement[i] = -1;
+            for(int j=0;j<i;j++) {
+                if(nums[i]%nums[j] == 0) {
+                    if(groupSize[i] < 1 + groupSize[j]) {
+                        groupSize[i] = 1 + groupSize[j];
+                        prevElement[i] = j;
+                    }
+                }
+            }
+            if(groupSize[i] > groupSize[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        List<Integer> result = new ArrayList();
+        while(maxIndex != -1) {
+            result.add(0, nums[maxIndex]);
+            maxIndex = prevElement[maxIndex];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {1,2,3,4};
+        System.out.println(new Solution().largestDivisibleSubset(nums));
+    }
+}
