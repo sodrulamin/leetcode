96bb4998755085289ce6632570739c2280f872c5
Sodrul Amin Shaon <msashawn.buet11@gmail.com>
2024-03-03 20:48:20 +0600

    19. Remove Nth Node From End of List - again

--- src/main/java/p19/Solution.java
+++ src/main/java/p19/Solution.java
@@ -1,81 +1,26 @@
 package p19;
 
-import data.ListNode;
-
-import java.util.List;
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 public class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head == null)
-            return null;
-        else if (head.next == null && n == 1)
-            return null;
-
-        ListNode current, deleteNode, prev = null;
-        current = head;
-        while (n > 0 && current != null){
-            n--;
+        ListNode current = head;
+        while (n-- > 1)
             current = current.next;
-        }
 
+        ListNode deleted = head;
+        ListNode previous = null;
 
-        if(n > 0)
-            return head;
-
-        deleteNode = head;
-        while (current != null){
+        while (current.next != null) {
             current = current.next;
-            prev = deleteNode;
-            deleteNode = deleteNode.next;
+            previous = deleted;
+            deleted = deleted.next;
         }
 
-        if(deleteNode == head){
-            head = head.next;
-        }
-        else {
-            prev.next = deleteNode.next;
-        }
-        deleteNode.next = null;
+        if(previous == null)
+            return head.next;
+
+        previous.next = deleted.next;
+        deleted.next = null;
 
         return head;
-    }
-    
-
-    public static void main(String[] args) {
-        int [] nums = {1, 2};
-        ListNode head = null;
-        ListNode current = null;
-        for(int i : nums){
-            if(head == null){
-                head = new ListNode(i);
-                head.next = null;
-
-                current = head;
-            }
-            else {
-                current.next = new ListNode(i, null);
-                current = current.next;
-            }
-        }
-
-        head = new Solution().removeNthFromEnd(head, 2);
-        System.out.print("[");
-        while (head != null){
-            System.out.print(head.val);
-            head = head.next;
-            if(head != null) System.out.print(",");
-        }
-
-        System.out.println("]");
     }
 }
---
+++ src/main/java/p19/SolutionOld.java
@@ -0,0 +1,79 @@
+package p19;
+
+import data.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class SolutionOld {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null)
+            return null;
+        else if (head.next == null && n == 1)
+            return null;
+
+        ListNode current, deleteNode, prev = null;
+        current = head;
+        while (n > 0 && current != null){
+            n--;
+            current = current.next;
+        }
+
+
+        if(n > 0)
+            return head;
+
+        deleteNode = head;
+        while (current != null){
+            current = current.next;
+            prev = deleteNode;
+            deleteNode = deleteNode.next;
+        }
+
+        if(deleteNode == head){
+            head = head.next;
+        }
+        else {
+            prev.next = deleteNode.next;
+        }
+        deleteNode.next = null;
+
+        return head;
+    }
+    
+
+    public static void main(String[] args) {
+        int [] nums = {1, 2};
+        ListNode head = null;
+        ListNode current = null;
+        for(int i : nums){
+            if(head == null){
+                head = new ListNode(i);
+                head.next = null;
+
+                current = head;
+            }
+            else {
+                current.next = new ListNode(i, null);
+                current = current.next;
+            }
+        }
+
+        head = new SolutionOld().removeNthFromEnd(head, 2);
+        System.out.print("[");
+        while (head != null){
+            System.out.print(head.val);
+            head = head.next;
+            if(head != null) System.out.print(",");
+        }
+
+        System.out.println("]");
+    }
+}
