99186a8f1ee2d767f6aee60b71d91561f68dd373
Sodrul Amin Shaon <msashawn.buet11@gmail.com>
2024-03-07 18:47:26 +0600

    141. Linked List Cycle

---
+++ src/main/java/p141/Solution.java
@@ -0,0 +1,17 @@
+package p141;
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        return visit(head, 0);
+    }
+
+    private boolean visit(ListNode node, int position) {
+        if(node == null)
+            return false;
+
+        if(position > 10000)
+            return true;
+
+        return visit(node.next, position + 1);
+    }
+}
