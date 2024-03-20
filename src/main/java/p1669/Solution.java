2724794d7fc2bfcbeb25200956a1ae282f540deb
Sodrul Amin Shaon <msashawn.buet11@gmail.com>
2024-03-20 12:04:59 +0600

    1669. Merge In Between Linked Lists

---
+++ src/main/java/p1669/Solution.java
@@ -0,0 +1,29 @@
+package p1669;
+
+public class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        int x = b - a + 2; //exclude both start and end, so add 2
+
+        ListNode first = list1;
+        while (a > 1) {
+            first = first.next;
+            a--;
+        }
+
+        ListNode last = first;
+        while (x > 0) {
+            last = last.next;
+            x--;
+        }
+
+        first.next = list2;
+        while (first.next != null) {
+            first = first.next;
+        }
+
+        first.next = last;
+
+        return list1;
+    }
+
+}
