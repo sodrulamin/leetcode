57a380348f9746ff42465c739374697ca3509e6d
Sodrul Amin Shaon <msashawn.buet11@gmail.com>
2024-03-22 15:05:36 +0600

    Done some testing

---
+++ src/test/java/GeneralTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class GeneralTest {
+    @Test
+    void stringEquals () {
+        String s1 = "hello";
+        String s2 = "hello";
+
+        System.out.println(s1 == s2);
+    }
+}
