506fd8540c0592934cedfb45e83fac8debad2e06
Sodrul Amin Shaon <msashawn.buet11@gmail.com>
2024-03-24 19:30:05 +0600

    287. Find the Duplicate Number

--- src/main/java/p287/Solution.java
+++ src/main/java/p287/Solution.java
@@ -1,6 +1,12 @@
 package p287;
 
 public class Solution {
+    /**
+     * Finds the duplicate entry of the given array using Floyd's cycle detection algorithm. The details of the
+     * algorithm can be found <a href="https://www.youtube.com/watch?v=LUm2ABqAs1w">here</a>
+     * @param nums contains numbers.
+     * @return the number which occurs more thane once.
+     */
     public int findDuplicate(int[] nums) {
         int fast = nums[0];
         int slow = nums[0];
